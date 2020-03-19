334c5c91b0808cb725c57079d4276c51d0a71178
Jery <oshindeveloper12@gmail.com>
2020-03-19 15:36:26 -0400

    This was a changing to try

--- src/components/PortfolioItem.js
+++ src/components/PortfolioItem.js
@@ -56,7 +56,7 @@ const PortfolioItem = () => {
                     <h2>{item.node.title}</h2>
                     <PortfolioImage src={item.node.featured_media.source_url} alt="Thumbnail" />
                     <div dangerouslySetInnerHTML={{__html: item.node.excerpt}} />
-                    <Link to={`/portfolio/${item.node.slug}`}>Read More</Link>
+                    <Link to={`/portfolio/${item.node.slug}`}>Read . . .</Link>
                 </PortfolioItems>
             ))
         }</PortfolioItemsWrapper>
